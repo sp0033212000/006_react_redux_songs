a9fcfbbfe2998d960658c534a00817babdcf2f9d
Liao Ji Wei <liaojiwei@Liaode-MacBook-Pro.local>
2019-12-03 01:14:51 +0800

    Updates

---
+++ precache-manifest.cd442b6a438a62593508941b4e24ea9a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a4e608eca829763d5bc6147d28cd6f6",
+    "url": "/006_react_redux_songs/index.html"
+  },
+  {
+    "revision": "8bfeb07b3a3326f5515c",
+    "url": "/006_react_redux_songs/static/js/2.c71cd6cd.chunk.js"
+  },
+  {
+    "revision": "217bc162873a5d933882",
+    "url": "/006_react_redux_songs/static/js/main.ae58e838.chunk.js"
+  },
+  {
+    "revision": "496d2416b0ba8a98c146",
+    "url": "/006_react_redux_songs/static/js/runtime-main.3229d50d.js"
+  }
+]);
